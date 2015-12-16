6369bd9994b862558b1d87dcec19dd5f3bdaab40
MasayukiSuda <topman31daasuu@gmail.com>
2015-12-16 19:56:38 +0900

    first commit

---
+++ library/src/main/java/com/daasuu/library/CountAnimationTextView.java
@@ -0,0 +1,117 @@
+package com.daasuu.library;
+
+import android.animation.Animator;
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
+
+
+public class CountAnimationTextView extends TextView {
+
+    private boolean isAnimating = false;
+    private ValueAnimator mCountAnimator;
+    private CountAnimationListener mCountAnimationListener;
+
+    private DecimalFormat mDecimalFormat;
+
+    private static final long DEFAULT_DURATION = 1000;
+
+    public CountAnimationTextView(Context context) {
+        this(context, null, 0);
+    }
+
+    public CountAnimationTextView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public CountAnimationTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        setUpAnimator();
+    }
+
+    private void setUpAnimator() {
+        mCountAnimator = new ValueAnimator();
+        mCountAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                String value;
+                if (mDecimalFormat == null) {
+                    value = String.valueOf(animation.getAnimatedValue());
+                } else {
+                    value = mDecimalFormat.format(animation.getAnimatedValue());
+                }
+                CountAnimationTextView.super.setText(value);
+            }
+        });
+
+        mCountAnimator.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                isAnimating = true;
+
+                if (mCountAnimationListener == null) return;
+                mCountAnimationListener.onAnimationStart(mCountAnimator.getAnimatedValue());
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                isAnimating = false;
+
+                if (mCountAnimationListener == null) return;
+                mCountAnimationListener.onAnimationEnd(mCountAnimator.getAnimatedValue());
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+                // do nothing
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+                // do nothing
+            }
+        });
+        mCountAnimator.setDuration(DEFAULT_DURATION);
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        if (mCountAnimator != null) {
+            mCountAnimator.cancel();
+        }
+    }
+
+    public void countAnimation(int fromValue, int toValue) {
+        if (isAnimating) return;
+
+        mCountAnimator.setIntValues(fromValue, toValue);
+        mCountAnimator.start();
+    }
+
+    public void setAnimationDuration(long duration) {
+        mCountAnimator.setDuration(duration);
+    }
+
+    // interface progress animationListener
+    public interface CountAnimationListener {
+        void onAnimationStart(Object animatedValue);
+
+        void onAnimationEnd(Object animatedValue);
+    }
+
+    public void setDecimalFormat(DecimalFormat mDecimalFormat) {
+        this.mDecimalFormat = mDecimalFormat;
+    }
+
+    public void clearDecimalFormat() {
+        this.mDecimalFormat = null;
+    }
+
+    public void setCountAnimationListener(CountAnimationListener mCountAnimationListener) {
+        this.mCountAnimationListener = mCountAnimationListener;
+    }
+}
---
+++ sample/src/main/java/com/daasuu/countanimationtextview/MainActivity.java
@@ -0,0 +1,64 @@
+package com.daasuu.countanimationtextview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.daasuu.library.CountAnimationTextView;
+
+import java.text.DecimalFormat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private CountAnimationTextView mCountAnimationTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mCountAnimationTextView = (CountAnimationTextView) findViewById(R.id.count_animation_textView);
+
+        Button button0 = (Button) findViewById(R.id.btn_count_anim_0);
+        button0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCountAnimationTextView.countAnimation(0, 99999);
+            }
+        });
+
+        Button button1 = (Button) findViewById(R.id.btn_count_anim_1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCountAnimationTextView.countAnimation(99999, 0);
+            }
+        });
+
+        Button button2 = (Button) findViewById(R.id.btn_count_anim_2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCountAnimationTextView.setDecimalFormat(new DecimalFormat("###,###,###"));
+                mCountAnimationTextView.countAnimation(0, 9999999);
+            }
+        });
+
+
+        mCountAnimationTextView.setAnimationDuration(10000);
+
+        mCountAnimationTextView.setCountAnimationListener(new CountAnimationTextView.CountAnimationListener() {
+            @Override
+            public void onAnimationStart(Object animatedValue) {
+                // do nothing
+            }
+
+            @Override
+            public void onAnimationEnd(Object animatedValue) {
+                mCountAnimationTextView.clearDecimalFormat();
+            }
+        });
+
+    }
+}
